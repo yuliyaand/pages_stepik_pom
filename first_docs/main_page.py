b9956c26cb550b9b35b1e25e3166f863c3bb1f81
yuliyaand <3440097@gmail.com>
2026-03-04 17:07:41 +0300

    Last login changes

--- first_docs/main_page.py
+++ first_docs/main_page.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 
 from .base_page import BasePage
 from .locators import MainPageLocators
+from .login_page import LoginPage
 
 class MainPage(BasePage):
 #таким образом, класс MainPage будет иметь доступ ко всем атрибутам и 
@@ -11,6 +12,7 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
+        return LoginPage(browser=self.browser, url=self.browser.current_url)
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
