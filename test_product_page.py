7faf7de87acbadffa2b3a7b28c6604b0b26fc5a9
yuliyaand <3440097@gmail.com>
2026-03-06 13:07:12 +0300

    Adding new product_page function test_guest_cant_see_product_in_basket_opened_from_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -2,6 +2,7 @@
 
 import pytest
 from .pages.product_page import ProductPage
+from .pages.basket_page import BasketPage
 
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -72,3 +73,11 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    basket_page = page.go_to_basket_page() # выполняем метод страницы — переходим на 
+    #страницу корзины
+    basket_page.should_be_empty_basket()
